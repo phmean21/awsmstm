3e08ad2ed0d46d9dbdc72f7efb4cb6b1dd6020ec
phmean21 <dima_zol16@mail.ru>
2018-12-30 17:23:17 +0300

    #12

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,22 @@
 |
 */
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
-    Route::get('/', 'DashboardController@dashboard')->name('admin.index');
-    Route::resource('/category', 'CategoryController', ['as' => 'admin']);
-});
+//Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
+//    Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+//    Route::resource('/category', 'CategoryController', ['as' => 'admin']);
+//});
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('rest', 'RestTestController')->names('RestTest');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
+    Route::resource('post', 'PostController')->names('blog.post');
+
+});
+Route::resource('rest', 'RestTestController')->names('RestTest');
+
