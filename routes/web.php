69ba25cd29216cfa7653ff8aaa8672fc86af7ced
phmean21 <dima_zol16@mail.ru>
2019-01-06 01:36:15 +0300

    init

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
+<<<<<<< HEAD
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
     Route::get('/', 'DashboardController@dashboard')->name('admin.index');
     Route::resource('/category', 'CategoryController', ['as' => 'admin']);
@@ -22,3 +23,33 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+=======
+//Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
+//    Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+//    Route::resource('/category', 'CategoryController', ['as' => 'admin']);
+//});
+Route::get('/', function () {
+    return view('welcome');
+});
+Auth::routes();
+Route::resource('rest', 'RestTestController')->names('RestTest');
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
+    Route::resource('post', 'PostController')->names('blog.post');
+
+});
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+    $methods = ['index', 'edit', 'store', 'update', 'create'];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
+//Route::resource('rest', 'RestTestController')->names('RestTest');
+
+>>>>>>> init
